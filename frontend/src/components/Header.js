491752eda8ab862024ff8f9f2638e871a7139827
stefanbauer <alternative2k@googlemail.com>
2021-09-04 15:24:18 +0200

    add header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export default function Header() {
+
+    return(
+        <Wrapper>
+            <img src="" alt="lets ride - logo"/>
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.header`
+  width: 100%;
+  text-align: center;
+  // image and background color
+`
